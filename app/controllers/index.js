f5ceffc0663fb776e68866eec7ffb3523f94e34b
Joanna <joanna@JCMUNDY.local>
2017-10-06 13:28:32 -0400

    functioning start

---
+++ app/controllers/index.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  responseMessage: '',
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('contact', { email: email });
+      newInvitation.save();
+
+      this.set('responseMessage', `Thank you! We have just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
+});
