aedb86cd9a65006848496f25043582f8d87d9c3d
Joanna <joanna@jcmundy.library.emory.ddns>
2017-10-10 15:15:53 -0400

    adapter error

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import AdminAdapterMixin from 'ember-admin/mixins/admin-adapter-mixin';
 
 const { String: { pluralize, underscore } } = Ember;
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(AdminAdapterMixin, {
 
   pathForType(type) {
     return pluralize(underscore(type));
